fb3a13a450ca9cb07fb2fcca728ecd4e46d2e410
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-07-23 14:17:14 -0300

    Subir responsive

---
+++ src/components/Header.jsx
@@ -0,0 +1,105 @@
+//React
+import { Link } from "react-router-dom"
+//Styles
+import styles from "../modules/Header.module.css"
+//Imagenes
+import logo from "../assets/img/logo.png"
+
+function Header() {
+    return (
+      <>
+         <header className={styles.header}>
+           <section  className={styles.logo}>
+             <img src={logo} alt="Logo" />
+             <input
+                placeholder=" Buscar"
+                type="text"
+                className={styles.buscador}
+              />
+            
+             <p  className={styles.burger}>
+             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+               <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+             </svg>
+             </p>
+              
+           </section>
+           <nav className={styles.nav}>
+            <ul>  
+                    <li>
+                       
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                      <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
+                      <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
+                    </svg>
+
+                        <Link className={styles.item}> 
+                           Inicio
+                        </Link>
+                    </li>
+                    <li>
+    
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                      <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                    </svg>
+
+                        <Link className={styles.item}> 
+                          Perfil
+                        </Link>
+                    </li>
+                    <li>
+                   
+                    <Link className={styles.item}> 
+                      Mi mascota
+                     </Link>
+                    </li>
+                    <li>
+                    
+                        <Link className={styles.item}> 
+                         Adopciones
+                        </Link>
+                        </li>
+                    <li>
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-check-fill" viewBox="0 0 16 16">
+                      <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-1.646-7.646-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+                    </svg>
+
+                        <Link className={styles.item}> 
+                        Pet Shop
+                        </Link>
+                        </li>
+                    <li> 
+                        
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+                      <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+                    </svg>
+
+                        <Link className={styles.item}> 
+                        Configuracion
+                        </Link>
+                        </li>
+ 
+                </ul>
+           </nav>
+           <section  className={styles.opciones}>
+                   
+            <Link className={styles.button_acceso_responsive} to={`/`}>
+               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="green" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>   
+               </Link>    
+       
+               <Link className={styles.button_acceso} to={`/`}>
+                  Iniciar Sesion
+                </Link>
+         
+            </section>
+         </header>
+      </>
+    )
+  }
+  
+  export default Header
+  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Header from "../components/Header"
+
 function Home() {
   return (
     <>
-      Home
+       <Header />
+       <div>
+         
+       </div>
     </>
   )
 }
