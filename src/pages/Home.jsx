166e40520043574b4e2cb704cd439eec75c61bd8
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-07-27 23:16:13 -0300

    home estructura

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,35 @@
+//Componentes
 import Header from "../components/Header"
+//Styles
+import styles from "../modules/Home.module.css"
 
 function Home() {
   return (
     <>
-       <Header />
-       <div>
-         
+       <Header /> 
+       <div className={styles.conteiner}> 
+         <section className={styles.leftsection}>
+           <div className={styles.perfilinfo}>
+             
+           </div>
+           <div className={styles.mapinfo}>
+            
+            </div>
+         </section>
+         <section className={styles.midsection}>
+             <div className={styles.crearpublicacion}>
+             
+             </div>
+             <div className={styles.posteo}>
+              
+             </div>
+         </section>
+        <section className={styles.rightsection}>
+
+         </section>
        </div>
     </>
   )
-}
+} 
 
 export default Home
